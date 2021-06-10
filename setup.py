06c1b4dcbbf92af2742b5a80b8c1b4fbc2899267
kevin.stanton <kevin.stanton@mottmac.com>
2021-06-09 21:00:48 -0700

    update version

--- setup.py
+++ setup.py
@@ -8,12 +8,11 @@ Created on Mon Apr 29 15:26:36 2019
 from setuptools import setup, find_packages
 
 setup(name='mmodpy',
-      version='1.0.1',
+      version='1.0.2',
       description='A collection of automation scripts to expedite LS-DYNA analytics.',
       url='https://github.com/mottmacdonaldglobal/mmodpy.git',
       author='Kevin Stanton',
       author_email='kevin.stanton@mottmac.com',
-      install_requires=['subprocess'],
       packages=find_packages(),
       etc_files=[('mmodpy/etc',
                    ['mmodpy/etc/Primer-Automesh_Coarse_15b.js',
