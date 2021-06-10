0168a759e3a81853f5d2d5e94386df8f114e6931
Kevin Stanton <77359542+STA94720@users.noreply.github.com>
2021-06-10 10:19:27 -0700

    Update __init__.py

--- mmodpy/beams/__init__.py
+++ mmodpy/beams/__init__.py
@@ -6,4 +6,4 @@ initPath = os.path.dirname(os.path.realpath(__file__))
 sys.path.insert(0, initPath)
 
 # Import project libraries
-from . import Beams, initialize
+from mmodpy.beams.Beams import MAT_HYSTERETIC_BEAM, MAT_ELASTIC
